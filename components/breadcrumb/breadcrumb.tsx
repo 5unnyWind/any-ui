d1a715bd2f05302cce248d95bb5ca16f61baf419
victinzhong <victinzhong@163.com>
2023-01-14 23:14:11 +0800

    尝试完成面包屑组件

---
+++ components/breadcrumb/breadcrumb.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+import classnames from "classnames";
+
+import BreadcrumbItem from "./breadcrumbItem";
+
+//单个路由信息定义
+export type Route = {
+  label: string;
+  routeUrl: string;
+  disabled: boolean;
+};
+
+//面包屑的传入参数定义
+interface defaultBreadcrumbProps {
+  itemRender: (
+    route: Route,
+    params: Object,
+    routes: Route[]
+  ) => React.ReactNode; //自定义链接函数
+  routes: Route[]; //路由信息组
+  separator: React.ReactNode;
+}
+
+type NativeBreadcrumbProps = defaultBreadcrumbProps &
+  React.BaseHTMLAttributes<HTMLElement>;
+
+export type BreadcrumbProps = Partial<NativeBreadcrumbProps>;
+
+const Breadcrumb: React.FC<BreadcrumbProps> = (props) => {
+  const { itemRender, routes, separator } = props;
+
+  return routes?.map((item, index) => {
+    return (
+      <>
+        <BreadcrumbItem {...item} />
+        {separator && index !== routes.length ? separator : ""}
+      </>
+    );
+  });
+};
+
+Breadcrumb.defaultProps = {
+  separator: false,
+};
+
+export default Breadcrumb;
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "../components/styles/index.scss";
 import ButtonDemo from "../components/button/demo/size";
+import Breadcrumb from "../components/breadcrumb/demo/base";
 
 function App() {
   return (
     <div className="App">
       <ButtonDemo></ButtonDemo>
+      <Breadcrumb></Breadcrumb>
     </div>
   );
 }
