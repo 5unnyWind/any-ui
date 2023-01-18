aca5c2f45866c4d509567bb2a90ee0cac3fda159
sunnywind <sunny-wind@foxmail.com>
2023-01-18 21:46:14 +0800

    init : ladle doc

--- components/index.ts
+++ components/index.ts
@@ -1,1 +1,4 @@
 export { default as Button } from "./button/button";
+export { default as Badge } from "./badge";
+
+import './styles/index.scss';
---
+++ src/badge.stories.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Badge } from "../components";
+
+export const Basic = () => (
+  <>
+    <h2>徽标</h2>
+    <Badge type="default">默认</Badge>
+    <Badge type="secondary">次要</Badge>
+    <Badge type="success">成功</Badge>
+    <Badge type="warning">警告</Badge>
+    <Badge type="error">失败</Badge>
+    <Badge type="default">35</Badge>
+    <Badge type="default" dot />
+  </>
+);
+
+export const anchor = () => (
+  <>
+    <h2>锚定徽标</h2>
+    <Badge.Anchor placement="topRight">
+      <Badge type="default">12</Badge>
+      <div
+        style={{
+          width: "50px",
+          height: "50px",
+          backgroundColor: "gray",
+          borderRadius: "10px",
+        }}
+      >
+        头像
+      </div>
+    </Badge.Anchor>
+  </>
+);
