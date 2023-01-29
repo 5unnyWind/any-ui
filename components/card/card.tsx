7236f91c9601f6285cd9a14149a8db5a42f40891
季晓曈 <aileenjitong@163.com>
2023-01-29 21:44:21 +0800

    add card component

---
+++ components/card/card.tsx
@@ -0,0 +1,66 @@
+import React, { Children, useRef, useState } from "react";
+import classNames from "classnames";
+
+export type cardSize = "default" | "small";
+
+export interface Cardprops
+  extends Omit<React.HTMLAttributes<HTMLDivElement>, "title"> {
+  className?: string;
+  size?: cardSize; //控制组件大小
+  title?: string; //传入的标题
+  bordered?: boolean; //控制是否有边框
+  headStyle?: React.CSSProperties;
+  style?: React.CSSProperties;
+  bodyStyle?: React.CSSProperties;
+  children?: React.ReactNode;
+  loading?: boolean;
+}
+const Card = React.forwardRef(
+  (props: Cardprops, ref: React.Ref<HTMLDivElement>) => {
+    console.log(props);
+    const [hasborder, setborder] = useState(true);
+    const {
+      size,
+      title,
+      bordered = {},
+      headStyle = {},
+      bodyStyle,
+      style,
+      children,
+      loading,
+      ...others
+    } = props;
+
+    const card = classNames("ai-card");
+    const headclass = classNames(
+      "ai-card-head",
+      hasborder ? "hasborder" : null,
+      title ? null : "istitle"
+    );
+    const headwrapperclass = classNames("ai-card-head-wrapper");
+    //    const headtitleclass=classNames()
+    const headbody = classNames(
+      title ? "ai-card-body" : "ai-card-notitle-body",
+      hasborder ? "hasborder" : null
+    );
+    const body = (
+      <div className={headbody} style={bodyStyle}>
+        {children}
+      </div>
+    );
+    return (
+      <div className={card} style={style}>
+        {/* head */}
+        <div className={headclass}>
+          <div className={headwrapperclass}>
+            {title && <div style={headStyle}>{title}</div>}
+          </div>
+        </div>
+        {/* body */}
+        {body}
+      </div>
+    );
+  }
+);
+
+export default Card;
--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "../components/styles/index.scss";
 import ButtonDemo from "../components/button/demo/size";
 import BadgeDemo from "../components/badge/demo/basic";
 import SwitchDemo from "../components/switch/demo/demo";
-
+import CardDemo from "../components/card/demo/basic";
 function App() {
   return (
     <div className="App">
@@ -11,6 +11,7 @@ function App() {
       <div />
       <BadgeDemo></BadgeDemo>
       <SwitchDemo></SwitchDemo>
+      <CardDemo></CardDemo>
     </div>
   );
 }
