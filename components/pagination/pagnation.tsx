ac358d1e4ba5528c7fd3d78e4e695365a639f2f3
Vsolon0401 <2390426297@qq.com>
2023-01-31 18:46:06 +0800

    feat: add pagination component

--- components/pagination/pagnation.tsx
+++ components/pagination/pagnation.tsx
@@ -24,100 +24,11 @@ const Pagination: React.FC<PaginationProps> = ({
   className,
   size,
   locale: customLocale,
-  selectComponentClass,
-  showSizeChanger,
   ...restProps
 }) => {
-  //
-  // const {
-  //   getPrefixCls,
-  //   direction,
-  //   pagination = {},
-  // } = React.useContext(ConfigContext);
-  // const prefixCls = getPrefixCls("pagination", customizePrefixCls);
-
-  // Style
-
-  // const mergedShowSizeChanger = showSizeChanger ?? pagination.showSizeChanger;
-
-  // const getIconsProps = () => {
-  //   const ellipsis = <span className={`${prefixCls}-item-ellipsis`}>•••</span>;
-  //   let prevIcon = (
-  //     <button className={`${prefixCls}-item-link`} type="button" tabIndex={-1}>
-  //       <LeftOutlined />
-  //     </button>
-  //   );
-  //   let nextIcon = (
-  //     <button className={`${prefixCls}-item-link`} type="button" tabIndex={-1}>
-  //       <RightOutlined />
-  //     </button>
-  //   );
-  //   let jumpPrevIcon = (
-  //     <a className={`${prefixCls}-item-link`}>
-  //       {/* You can use transition effects in the container :) */}
-  //       <div className={`${prefixCls}-item-container`}>
-  //         <DoubleLeftOutlined className={`${prefixCls}-item-link-icon`} />
-  //         {ellipsis}
-  //       </div>
-  //     </a>
-  //   );
-  //   let jumpNextIcon = (
-  //     <a className={`${prefixCls}-item-link`}>
-  //       {/* You can use transition effects in the container :) */}
-  //       <div className={`${prefixCls}-item-container`}>
-  //         <DoubleRightOutlined className={`${prefixCls}-item-link-icon`} />
-  //         {ellipsis}
-  //       </div>
-  //     </a>
-  //   );
-  //   // change arrows direction in right-to-left direction
-  //   if (direction === "rtl") {
-  //     [prevIcon, nextIcon] = [nextIcon, prevIcon];
-  //     [jumpPrevIcon, jumpNextIcon] = [jumpNextIcon, jumpPrevIcon];
-  //   }
-  //   return {
-  //     prevIcon,
-  //     nextIcon,
-  //     jumpPrevIcon,
-  //     jumpNextIcon,
-  //   };
-  // };
   const extendedClassName = classNames("pgn", {}, className);
 
-  return (
-    <RcPagination {...restProps} className={extendedClassName} />
-    // (contextLocale: any) => {
-    //   const locale = { ...contextLocale, ...customLocale };
-    //   const isSmall = size === "small" || !!(xs && !size && responsive);
-    //   const selectPrefixCls = getPrefixCls(
-    //     "select",
-    //     customizeSelectPrefixCls
-    //   );
-    //   const extendedClassName = classNames(
-    //     {
-    //       [`${prefixCls}-mini`]: isSmall,
-    //       [`${prefixCls}-rtl`]: direction === "rtl",
-    //     },
-    //     className,
-    //     hashId
-    //   );
-    //
-    //   return wrapSSR(
-    //     <RcPagination
-    //       {...getIconsProps()}
-    //       {...restProps}
-    //       prefixCls={prefixCls}
-    //       selectPrefixCls={selectPrefixCls}
-    //       className={extendedClassName}
-    //       selectComponentClass={
-    //         selectComponentClass || (isSmall ? MiniSelect : MiddleSelect)
-    //       }
-    //       locale={locale}
-    //       showSizeChanger={mergedShowSizeChanger}
-    //     />
-    //   );
-    // }
-  );
+  return <RcPagination {...restProps} className={extendedClassName} />;
 };
 
 if (process.env.NODE_ENV !== "production") {
