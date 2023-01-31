b1ebf12e8e8b8a0f1b28bf2b46b1a898de7eb49b
GitHub hqj678@gmail.com <GitHub hqj678@gmail.com>
2023-01-31 13:01:11 +0800

    fix: 修复bug

--- components/switch/switch.tsx
+++ components/switch/switch.tsx
@@ -6,11 +6,11 @@ interface BaseSwitchProps {
   disabled?: boolean; // 是否开启禁用
   width?: string; // 控制组件大小
   defaultChecked?: boolean; // 控制是否
-  activeColor?: [boolean, string, number];
-  inactiveColor?: [boolean, string, number];
-  activeValue?: string;
-  inactiveValue?: string;
-  children?: React.ReactNode;
+  activeColor?: string; //控制打开颜色
+  inactiveColor?: string; //控制关闭颜色
+  activeValue?: string; //打开值
+  inactiveValue?: string; //关闭值
+  children?: React.ReactNode; //传入子
 }
 
 const Switch: React.FC<BaseSwitchProps> = (props) => {
@@ -70,7 +70,7 @@ const Switch: React.FC<BaseSwitchProps> = (props) => {
 
 Switch.defaultProps = {
   disabled: false,
-  width: 40,
+  width: "40",
   activeColor: "rgb(22, 119, 255)",
   inactiveColor: "rgb(191, 191, 191)",
   defaultChecked: true,
