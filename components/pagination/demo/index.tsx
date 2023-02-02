99893da6f3c4a1dad5189c5fe4ed7cf8bc3eaf41
Vsolon0401 <2390426297@qq.com>
2023-02-02 09:19:35 +0800

    fix:style

--- components/pagination/demo/index.tsx
+++ components/pagination/demo/index.tsx
@@ -2,7 +2,12 @@ import * as React from "react";
 import Pagination from "../pagnation";
 
 export const PaginationDemo: React.FC = () => {
-  return <Pagination total={200} />;
+  return (
+    <>
+      <Pagination total={500} />
+      <Pagination total={500} simple />
+    </>
+  );
 };
 
 export default PaginationDemo;
